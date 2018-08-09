9dad67af8a8afacff2e2a9d9bb9c93a989a1f128
1iyc <liycliycliyc@gmail.com>
2018-08-09 17:58:10 +0900

    Developing Vue&Express CRUD

---
+++ vue-front/src/components/IndexQna.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="qnas">
+    <h1>전체 QnA</h1>
+    <div v-for="qna in qnas" :key="qna.id" class="qna">
+      {{qna.id}} {{qna.main_question}} <br> {{qna.answer}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  created () {
+    this.$http.get('/qna')
+      .then((response) => {
+        this.qnas = response.data
+      })
+  },
+  data () {
+    return {
+      qnas: []
+    }
+  }
+}
+</script>
--- vue-front/src/router/index.js
+++ vue-front/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import IndexQna from '@/components/IndexQna'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/qna',
+      name: 'IndexQna',
+      component: IndexQna
     }
   ]
 })
